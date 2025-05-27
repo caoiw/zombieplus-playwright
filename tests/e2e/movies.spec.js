eeb9e7a4ba8e407ac24ba08adf67aa1bd7ccdedb
Caio William dos Santos <caio.santos@glok.com.br>
2025-05-27 15:20:59 -0300

    Atualização de cover dos filmes

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -8,19 +8,13 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
     const movie = data.create
     await executeSQL(`DELETE from movies WHERE title = '${movie.title}';`)
 
-    await page.login.visit()
-    await page.login.submit('admin@zombieplus.com', 'pwd123')
-    await page.login.isLoggedIn()
-
-    await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year)
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.create(movie)
     await page.toast.containText('Cadastro realizado com sucesso!')
 })
 
 test('não deve cadastrar quando os campos obrigatórios não são preenchidos', async ({ page }) => {
-    await page.login.visit()
-    await page.login.submit('admin@zombieplus.com', 'pwd123')
-    await page.login.isLoggedIn()
-
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
     await page.movies.goForm()
     await page.movies.submit()
 
